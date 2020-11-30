7c545accaeb7a88197683ce3ab313cdc1a2bcf9c
matsuo <mitsuru.matsuo.great@gmail.com>
2020-12-01 07:36:27 +0900

    second commit

--- Q06/Main.java
+++ Q06/Main.java
@@ -6,16 +6,14 @@ public class Main{
 		//文字数をもとにchar配列を作成(配列変数名chars)
 		char[] chars=new char[word.length()];
 		for(int i=0;i<chars.length;i++){
-			//配列にお尻の文字から詰めていく処理
-			お尻から詰めるには、文字数-1-i
-			//chars[i]=word.charAt(i);
+			//配列にお尻の文字から詰めていく処理、尻から詰めるには、文字数-1-i
+			chars[i]=word.charAt(chars.length-1-i);
 		}
 		System.out.println(Arrays.toString(chars));
 	}
 }
 
-
-import java.util.*;
+/*
 public class Main7{
   public static void main(String[] args){
     System.out.print("単語を入れてね>");
@@ -27,4 +25,6 @@ public class Main7{
     }
 	}
 }
+*/
+
 
